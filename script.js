71d24a6c4fa4e253d4cef862f87745b71ee4c610
Krishna Kumar <86869782+GitHubKrishnaKumar@users.noreply.github.com>
2023-01-11 23:58:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function threeSum(arr, target) {
   //your code here
+	let len = arr.length;
+	let sum =0;
+	for(let i=0;i<len;i++){
+		if(arr[i]==target){
+		  sum = arr[i]+arr[i-1]+arr[i-2];
+		}
+	}
+	console.log(sum);
 }
-
 module.exports = threeSum;
